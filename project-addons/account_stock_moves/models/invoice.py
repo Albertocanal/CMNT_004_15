6336dea585129ced28704d6662d342941c39b9ca
pmoreno-visiotech <pmoreno@visiotechsecurity.com>
2021-06-14 16:08:41 +0200

    [IMP] Actualizado cost_price2 cuando se cnacela la factura

--- project-addons/account_stock_moves/models/invoice.py
+++ project-addons/account_stock_moves/models/invoice.py
@@ -11,13 +11,12 @@ class AccountInvoice(models.Model):
             for line_data in res:
                 i_line_id = line_data['invl_id']
                 i_line = self.env['account.invoice.line'].browse(i_line_id)
-                if i_line.move_line_ids and \
-                    i_line.purchase_line_id and \
-                        i_line.product_id.valuation == 'real_time':
+                import ipdb
+                ipdb.set_trace()
+                if i_line.move_line_ids and i_line.purchase_line_id and i_line.product_id.valuation == 'real_time':
                     purchase_line = i_line.purchase_line_id
                     qty = i_line.quantity
-                    if i_line.product_id.cost_method in ['average', 'fifo'] \
-                            and purchase_line.price_unit:
+                    if i_line.product_id.cost_method in ['average', 'fifo'] and purchase_line.price_unit:
                         price_unit = purchase_line.price_unit * (1-(purchase_line.discount/100))
                         purchase_price = price_unit * qty
                     else:
@@ -30,8 +29,7 @@ class AccountInvoice(models.Model):
                         x.picking_type_code == 'incoming' and
                         x.state not in ('draft', 'cancel'))
                     if len(move_lines) > 1:
-                        line_data['create_date'] = \
-                            i_line.move_line_ids[0]._get_origin_create_date()
+                        line_data['create_date'] = i_line.move_line_ids[0]._get_origin_create_date()
                     else:
                         line_data['create_date'] = move_lines.create_date
             res.extend(self._cost_diff_move_lines(res))
@@ -42,6 +40,8 @@ class AccountInvoice(models.Model):
         diff_res = []
         # calculate and write down the possible price difference
         # between invoice price and product price
+        import ipdb
+        ipdb.set_trace()
         for line in res:
             i_line_id = line['invl_id']
             i_line = self.env['account.invoice.line'].browse(i_line_id)
@@ -51,19 +51,16 @@ class AccountInvoice(models.Model):
             if not acc:
                 # if not found on the product get the price difference
                 # account at the category
-                acc = i_line.product_id.categ_id.\
-                    property_account_creditor_price_difference_categ
+                acc = i_line.product_id.categ_id.property_account_creditor_price_difference_categ
             acc = fpos.map_account(acc).id
-            a = i_line.product_id.product_tmpl_id.\
-                get_product_accounts(fiscal_pos=fpos)['stock_input'].id
-            if 'purchase_line' in line \
-                    and line['purchase_line'].currency_id.id != i_line.invoice_id.currency_id.id:
-                line['price_purchase'] = line['purchase_line'].currency_id. \
-                    with_context(date=line['create_date']). \
-                    compute(line['price_purchase'], i_line.invoice_id.currency_id, round=True)
+            a = i_line.product_id.product_tmpl_id.get_product_accounts(fiscal_pos=fpos)['stock_input'].id
+            import ipdb
+            ipdb.set_trace()
+            if 'purchase_line' in line and line['purchase_line'].currency_id.id != i_line.invoice_id.currency_id.id:
+                line['price_purchase'] = line['purchase_line'].currency_id.with_context(date=line['create_date']).compute(line['price_purchase'], i_line.invoice_id.currency_id, round=True)
             price_subtotal = i_line.price_subtotal
-            if 'price_purchase' in line and acc and \
-                    tools.float_compare(line['price_purchase'], price_subtotal, 2):
+            ipdb.set_trace()
+            if 'price_purchase' in line and acc and tools.float_compare(line['price_purchase'], price_subtotal, 2):
                 price_diff = price_subtotal - line['price_purchase']
                 if tools.float_is_zero(price_diff, 2):
                     continue
@@ -99,3 +96,26 @@ class AccountInvoice(models.Model):
                     'account_analytic_id': line['account_analytic_id']
                     })
         return diff_res
+
+    @api.multi
+    def action_cancel(self):
+        import ipdb
+        ipdb.set_trace()
+        for i_line in self.invoice_line_ids:
+            i_line['price_purchase'] = i_line['purchase_line'].currency_id.with_context(date=line['create_date']).compute(line['price_purchase'], i_line.invoice_id.currency_id, round=True)
+            price_subtotal = i_line.price_subtotal
+            ipdb.set_trace()
+            if 'price_purchase' in line and acc and tools.float_compare(line['price_purchase'], price_subtotal, 2):
+                price_diff = price_subtotal - line['price_purchase']
+                if tools.float_is_zero(price_diff, 2):
+                    continue
+                else:
+                    for move in line.move_line_ids:
+                        move.write({
+                            'value': move.value + move.product_qty * price_diff,
+                            'remaining_value': move.remaining_value + move.remaining_qty * price_diff,
+                            'price_unit': move.price_unit + price_diff
+                        })
+                        move.product_id.product_tmpl_id.recalculate_standard_price_2()
+        return super(AccountInvoice, self).action_cancel()
+
